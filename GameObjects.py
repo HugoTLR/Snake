ece68f7bc8710b84dc888bdba06e98f1866e668e
vV <hugoruellet@gmail.com>
2020-09-17 12:21:09 +0200

    Base working

--- GameObjects.py
+++ GameObjects.py
@@ -29,13 +29,14 @@ class Board:
   def is_border(self,j,i): return j == 0 or j == self.H - 1 or i == 0 or i == self.W -1
 
   def update(self):
-    for j in range(1, self.H - 1, 1):
-      for i in range(1, self.W - 1, 1):
-        if (j,i) in self.snake.body:
+    for j in range(self.H):
+      for i in range(self.W):
+        if (j,i) == self.snake.dead_cell: self.board[j][i] = 5
+        elif self.is_border(j,i):         self.board[j][i] = 1
+        elif (j,i) in self.snake.body:
           if (j,i) == self.snake.body[0]:   self.board[j][i] = 3
           else:                             self.board[j][i] = 2
         elif (j,i) == self.food:            self.board[j][i] = 4
-        elif (j,i) == self.snake.dead_cell: self.board[j][i] = 5
         else:                               self.board[j][i] = 0
 
   def generate_food(self):
@@ -46,13 +47,53 @@ class Board:
 
 
 class Snake:
-  def __init__(self,parent):
+  MOVE_COST = 1
+  FOOD_REWARD = 10
+  def __init__(self,parent,timeleft = 150):
     self.parent = parent
+    self.body = self.init_body()
 
     self.dead_cell = None
-    self.body = self.init_body()
+
+    self.direction = random.randint(0,3) #4 directions possible
+
+    self.timeleft = 150 #How many moves we can do without eating food
+
+    self.livetime = 0 #How many step we've done
+
+
 
   def init_body(self):
     w2 = int(self.parent.W/2)
     h2 = int(self.parent.H/2)
     return [(h2,w2),(h2+1,w2),(h2+2,w2)]
+
+  def move(self):
+    #get_head
+    cur_head = self.body[0]
+
+    if self.direction == 0:   self.body.insert(0,(cur_head[0], cur_head[1] - 1 )) #Moving left
+    elif self.direction == 1: self.body.insert(0,(cur_head[0] - 1, cur_head[1] )) #Moving Up
+    elif self.direction == 2: self.body.insert(0,(cur_head[0], cur_head[1] + 1)) #Moving Right
+    else:               self.body.insert(0,(cur_head[0] + 1, cur_head[1])) #Moving Down
+
+
+    n_head = self.body[0]
+    self.livetime += 1
+    if n_head != self.parent.food:
+      self.body.pop()
+      self.timeleft -= Snake.MOVE_COST
+    else:
+      self.timeleft += Snake.FOOD_REWARD
+      self.parent.food = self.parent.generate_food()
+
+
+
+    #Handle end game
+    if self.body.count(n_head) > 1 or self.parent.is_border(n_head[0],n_head[1]): #We run into ourself or the border
+      self.dead_cell = n_head
+      self.timeleft = 0
+
+  def change_dir(self,direction):
+    self.direction = direction
+
--- main.py
+++ main.py
@@ -2,14 +2,28 @@ import GameObjects
 from Utils import Display
 import cv2 as cv
 
+
 width = 30 
 height = 20
 scale = 20  #Render scale factor
 delay = 0   #cv.waitKey delay
-di = Display.Display(delay,width,height,scale)
-b = GameObjects.Board(width = width, height = height)
 
-ui = di.draw(b)
-di.show(a=[ui])
+def run_test():
 
-cv.destroyAllWindows()
+  di = Display.Display(delay,width,height,scale)
+  b = GameObjects.Board(width = width, height = height)
+  while b.snake.dead_cell is None:
+
+    b.snake.move()
+    b.update()
+
+
+    ui = di.draw(b)
+    # print(b)
+    if di.show(a=[ui]):
+      break
+
+  cv.destroyAllWindows()
+
+
+run_test()
